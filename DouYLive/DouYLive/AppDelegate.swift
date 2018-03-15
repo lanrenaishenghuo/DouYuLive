e339674929cb4f0d50288216842a429ecb75e024
李超 <946491856@qq.com>
2018-03-15 14:14:57 +0800

    初始化项目

---
+++ DouYLive/DouYLive/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  DouYLive
+//
+//  Created by 东亨 on 2018/3/15.
+//  Copyright © 2018年 lichao. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UITabBar.appearance().tintColor = UIColor.orange
+        return true
+    }
+
+}
+
---
+++ DouYLive/DouYLive/Classes/Main/Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  DouYLive
+//
+//  Created by 东亨 on 2018/3/15.
+//  Copyright © 2018年 lichao. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChildVc(storeName: "Home")
+        addChildVc(storeName: "Live")
+        addChildVc(storeName: "Folow")
+        addChildVc(storeName: "Profile")
+    }
+    private func addChildVc(storeName:String){
+        let childVc = UIStoryboard(name:storeName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVc)
+    }
+}
